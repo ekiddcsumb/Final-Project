498fadd04f46cc86d9aa1b3be826ee8ffedffc43
emkidd <ekidd@csumb.edu>
2021-04-17 18:09:12 -0700

    Player movement

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -5,16 +5,15 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     public float mouseSensitivity;
-    public Transform playerBody;
     private float xRot = 0f;
     
-    // Start is called before the first frame update
+    public Transform playerBody;
+    
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
